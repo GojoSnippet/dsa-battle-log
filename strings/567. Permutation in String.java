09db868b6ccd6698bc53937eb90d15a497058d87
GojoSnippet <sdomakon@buffalo.edu>
2025-08-17 04:12:17 -0400

    Update 567. Permutation in String.java

--- strings/567. Permutation in String.java
+++ strings/567. Permutation in String.java
@@ -1,23 +1,25 @@
-UUse int array next time
-
 class Solution {
+    public boolean isEqual(int[] a,int[] b){
+        for(int i=0;i<a.length;++i)
+        if(a[i] != b[i])return false;
+        return true;
+    }
     public boolean checkInclusion(String s1, String s2) {
-        Map<Character,Integer> mymap = new HashMap<>();
-        for(Character s: s1.toCharArray())mymap.put(s,mymap.getOrDefault(s,0)+1);
-        int l1 = s1.length();
-        int l2 = s2.length();
-        if(l1 > l2)return false;
-        int startIndex=0;int lastIndex = l1-1;
-        Map<Character,Integer> newmap = new HashMap<>();
-        for(int i=0;i<l1;++i)newmap.put(s2.charAt(i),newmap.getOrDefault(s2.charAt(i),0)+1);
-        if(mymap.equals(newmap))return true;
-        while(lastIndex+1 <= l2-1){
-            Character c =s2.charAt(startIndex);
-            if(newmap.get(c)== 1)newmap.remove(c);
-            else newmap.put(c,newmap.get(c)-1);
-            newmap.put(s2.charAt(lastIndex+1),newmap.getOrDefault(s2.charAt(lastIndex+1),0)+1);
-            if(newmap.equals(mymap))return true;
-            startIndex++;lastIndex++;
+        if(s1.length() > s2.length())return false;
+        int window_size = s1.length();
+        int[] s1Array = new int[26];
+        int[] s2Array = new int[26];
+        for(Character c : s1.toCharArray())s1Array[c - 'a']++;
+        for(int i=0;i<window_size;++i){
+            s2Array[s2.charAt(i)-'a']++;
+        }
+        int start = 0;int end = window_size -1;
+        while(end < s2.length()){
+            if(isEqual(s1Array,s2Array))return true;
+            s2Array[s2.charAt(start) - 'a'] --;
+            start++;
+            end++;
+            if(end< s2.length()) s2Array[s2.charAt(end) - 'a']++;
         }
         return false;
     }
