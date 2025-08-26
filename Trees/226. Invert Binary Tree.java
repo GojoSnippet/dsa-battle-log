2ef52dae959bd09334fc5e917d4afbedaba01be5
GojoSnippet <sdomakon@buffalo.edu>
2025-08-25 23:55:09 -0400

    Create 226. Invert Binary Tree.java

---
+++ Trees/226. Invert Binary Tree.java
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if(root == null)return null;
+        TreeNode left_root  = invertTree(root.left);
+        TreeNode right_root = invertTree(root.right);
+        root.right = left_root;
+        root.left = right_root;
+        return root;
+    }
+}
