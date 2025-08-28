61b3d4b0520f174940cac03053dec09fc01864a5
GojoSnippet <sdomakon@buffalo.edu>
2025-08-28 19:20:17 -0400

    Update 102. Binary Tree Level Order Traversal.java

--- Trees/102. Binary Tree Level Order Traversal.java
+++ Trees/102. Binary Tree Level Order Traversal.java
@@ -14,21 +14,23 @@
  * }
  */
 class Solution {
-    public int calcHeight(TreeNode root){
-        if(root== null)return 0;
-        return 1+ Math.max(calcHeight(root.left),calcHeight(root.right));
-    }
-    List<List<Integer>> order_func(TreeNode root,List<List<Integer>> final_list,int level){
-        if(root == null)return final_list;
-        final_list.get(level).add(root.val);
-        final_list = order_func(root.left,final_list,level+1);
-        return order_func(root.right,final_list,level+1);
-    }
     public List<List<Integer>> levelOrder(TreeNode root) {
-        int height = calcHeight(root);
-        System.out.println(height);
-        List<List<Integer>> final_list = new ArrayList<>(height);
-        for(int i=0;i<height;++i)final_list.add(new ArrayList<>());
-        return order_func(root,final_list,0);
+        Queue<TreeNode> levelQueue = new LinkedList<>();
+        levelQueue.add(root);
+        List<List<Integer>> final_list= new ArrayList<>();
+        while(!levelQueue.isEmpty()){
+            List<Integer> levelList = new ArrayList<>();
+            for(int i=levelQueue.size();i>0;--i){
+                TreeNode temp = levelQueue.remove();
+                if(temp != null){
+                levelList.add(temp.val);
+                levelQueue.add(temp.left);
+                levelQueue.add(temp.right);
+                }
+            }
+            if(levelList.size() > 0)final_list.add(levelList);
+        }
+        return final_list;
+
     }
 }
